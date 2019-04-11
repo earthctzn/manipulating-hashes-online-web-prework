86f3f9c31afbe0fdd244b8b9f7fd57485e15be4d
Caleb Einarr <earthctzn1@gmail.com>
2019-04-11 03:25:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,8 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-      favorite_icecream_flavors.delete_if |flavor| = "strawberry"
+      value.delete_if do |flavor| 
+        flavor = "strawberry"
 
 
   #remember to return your newly altered contacts hash!
